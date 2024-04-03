e041b3e3b1a446c3918896cd6097ed56954fe9c2
Andrii Zemliankin <andrii.zemliankin@pptexcellence.com>
2024-04-03 22:42:50 +0200

    Add menu component and update scroll behavior

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,31 @@ import './App.css';
 import { Experience } from './components/Experience';
 import { Scroll, ScrollControls } from '@react-three/drei';
 import { Interface } from './components/Interface';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ScrollManager } from './components/ScrollManager';
+import { Menu } from './components/Menu';
 
 export const App = () => {
     const [section, setSection] = useState(0);
+    const [menuOpened, setMenuOpened] = useState(false);
+
+    useEffect(() => {
+        setMenuOpened(false);
+    }, [section]);
 
     return (
-        <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-            <color attach='background' args={['#f0f0f0']} />
-            <ScrollControls pages={4} damping={0.1}>
-                <ScrollManager section={section} onSectionChange={setSection} />
-                <Experience />
-                <Scroll html>
-                    <Interface />
-                </Scroll>
-            </ScrollControls>
-        </Canvas>
+        <>
+            <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+                <color attach='background' args={['#f0f0f0']} />
+                <ScrollControls pages={4} damping={0.1}>
+                    <ScrollManager section={section} onSectionChange={setSection} />
+                    <Experience />
+                    <Scroll html>
+                        <Interface />
+                    </Scroll>
+                </ScrollControls>
+            </Canvas>
+            <Menu onSectionChange={setSection} menuOpened={menuOpened} setMenuOpened={setMenuOpened} />
+        </>
     );
 };
--- src/components/ScrollManager.jsx
+++ src/components/ScrollManager.jsx
@@ -1,3 +1,48 @@
-export const ScrollManager = ({section, onSectionChange}) => {
-  return null
-}
+import { useScroll } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import gsap from 'gsap';
+import { useEffect, useRef } from 'react';
+
+export const ScrollManager = ({ section, onSectionChange }) => {
+    const data = useScroll();
+    const lastScroll = useRef(0);
+    const isAnimation = useRef(false);
+
+    data.fill.classList.add('top-0');
+    data.fill.classList.add('absolute');
+
+    useEffect(() => {
+        gsap.to(data.el, {
+            duration: 1,
+            scrollTop: section * data.el.clientHeight,
+            onStart: () => {
+                isAnimation.current = true;
+            },
+            onComplete: () => {
+                isAnimation.current = false;
+            },
+        });
+    }, [section]);
+
+    useFrame(() => {
+        // If the user is scrolling, we don't want to trigger the section change
+        if (isAnimation.current) {
+            lastScroll.current = data.scroll.current;
+            return;
+        }
+        // If the user has stopped scrolling, we can trigger the section change
+        const curSection = Math.floor(data.scroll.current * data.pages);
+        if (data.scroll.current > lastScroll.current && curSection === 0) {
+            onSectionChange(1);
+        }
+        // If the user is scrolling up and the current scroll position is less than the previous scroll position
+        // and the current scroll position is less than 1 divided by the total number of pages minus 1,
+        // then trigger the section change to the previous section.
+        if (data.scroll.current < lastScroll.current && data.scroll.current < 1 / (data.pages - 1)) {
+            onSectionChange(0);
+        }
+
+        lastScroll.current = data.scroll.current;
+    });
+    return null;
+};

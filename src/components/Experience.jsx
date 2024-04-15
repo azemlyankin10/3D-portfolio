b3e88e7cc747b943d99c3f1ffcd0fb325091b0df
Andrii Zemliankin <andrii.zemliankin@pptexcellence.com>
2024-04-15 21:28:54 +0200

    Add "wireframe" to cSpell.words in .vscode/settings.json, update package.json dependencies, add Avatar component and useControls hook in Experience component, and remove unnecessary code in Office component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,16 +1,26 @@
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, Sky } from '@react-three/drei';
 import { motion } from 'framer-motion-3d';
 import { Office } from './Office';
 import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import { useThree } from '@react-three/fiber';
+import { Avatar } from './Avatar';
+import { useControls } from 'leva';
 
-export const Experience = ({section}) => {
+export const Experience = ({ section }) => {
+    const { animation } = useControls({
+        animation: {
+            value: 'Typing',
+            options: ['Typing', 'Standing', 'Falling'],
+        },
+    });
     return (
         <>
-            <OrbitControls />
+            {/* <OrbitControls /> */}
+            {/* <Sky /> */}
             <ambientLight intensity={1} />
-            <motion.group animate={{y: section === 0 ? 0 : -1}}>
+            <motion.group animate={{ y: section === 0 ? 0 : -1 }}>
+                <Avatar animationName={animation} />
                 <Office position={[1, -1.3, 0]} scale={[0.6, 0.5, 0.5]} rotation={[1.2, 1.4, -1]} />
             </motion.group>
         </>
